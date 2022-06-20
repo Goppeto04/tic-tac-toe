9ed1739272c70f7ca438633c32d7978040a8ae4f
Goppeto04 <86419285+Goppeto04@users.noreply.github.com>
2022-06-20 17:06:45 +0300

    Initial commit

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameScript : MonoBehaviour {
+
+    public Text turn;
+    int spriteIndex = -1;
+
+    public int PlayerTurn()
+    {
+        spriteIndex++;
+        spriteIndex %= 2;
+
+        if (spriteIndex == 0)
+        {
+            turn.text = "O player turn";
+        }
+        else
+        {
+            turn.text = "X player turn";
+        }
+
+        return spriteIndex;
+    }
+}
